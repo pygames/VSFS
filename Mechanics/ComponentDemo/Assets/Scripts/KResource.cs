aa05ec24c2c214c5ed09873527b337c2c0e2484a
Elliot Miiler <jem060@bucknell.edu>
2018-10-16 14:01:58 -0700

    Sorry this is so late

---
+++ Mechanics/ComponentDemo/Assets/Scripts/KResource.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KResource : MonoBehaviour {
+
+    public GameObject[] prereqs;
+    public int activeTime;
+    public int passiveTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
