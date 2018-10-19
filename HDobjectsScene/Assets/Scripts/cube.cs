bb9eb645168494d980982118c38ee8110b7eaa8d
pygames <43159471+pygames@users.noreply.github.com>
2018-10-19 14:18:12 -0500

    SoupObjectsScene

---
+++ HDobjectsScene/Assets/Scripts/cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+    public float moveSpeed;
+    private Rigidbody rbody;
+	// Use this for initialization
+	void Start () {
+        moveSpeed = 5f;
+        rbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, moveSpeed * Input.GetAxis("Jump") * Time.deltaTime, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+
+    }
+}
